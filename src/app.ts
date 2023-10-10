0b0d292aabe59fec40a449ec3fc77bef0cfba230
rodolfo <rodolfojaques13@gmail.com>
2023-10-10 17:21:25 -0300

    fix: Modificação no retorno da rota login, agora passa o user alem do tokem

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import "express-async-errors"
 import express from "express"
 import globalErrorMiddleware from "./middlewares/globalError.middleware"
 import { appRoutes } from "./routes"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
