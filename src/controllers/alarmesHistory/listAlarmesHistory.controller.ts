f60ff2f22ff1edf6c759c9be03f0d3650f162f54
unknown <joaobuga35@gmail.com>
2023-10-31 11:10:49 -0300

    feat: filtro com query sql testado e comprovado

--- src/controllers/alarmesHistory/listAlarmesHistory.controller.ts
+++ src/controllers/alarmesHistory/listAlarmesHistory.controller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from "express";
 import listAlarmesHistoryService from "../../services/alarmesHistory/listHistoryAlarmes.service";
 import { AppError, handleError } from "../../error/appError";
-import { Pool, QueryConfig } from "pg";
-import queryBuilderParamsService from "../../services/alarmesHistory/queryHistory.service";
 
 const listAlarmesHistoryController = async (req: Request, res: Response) => {
   try {
     const { TIPO_TA, TIPO_REDE } = req.body;
-    const alarmes = await listAlarmesHistoryService(TIPO_TA, TIPO_REDE);
+    const alarmes = await listAlarmesHistoryService(req.body);
 
     return res.json(alarmes);
   } catch (error) {
--- src/services/alarmesHistory/listHistoryAlarmes.service.ts
+++ src/services/alarmesHistory/listHistoryAlarmes.service.ts
@@ -1,17 +1,41 @@
-import { EntityManager, getManager } from "typeorm";
 import { AlarmesHistory } from "../../../src-new/entities/alarmes.entity";
 import { AppDataSourceHistory } from "../../data-source";
 
-const listAlarmesHistoryService = async (
-  TIPO_TA: string,
-  TIPO_REDE: string
-) => {
+const listAlarmesHistoryService = async (data: any) => {
+  const {
+    TIPO_TA,
+    TIPO_REDE,
+    ESTADO,
+    LOCALIDADE,
+    SITE,
+    TIPO_ALARME,
+    CLASSIFICACAO,
+    DATA_INICIO,
+    DATA_FIM,
+  } = data;
   const alarmesRepository = AppDataSourceHistory.getRepository(AlarmesHistory);
-  const query = `SELECT * FROM alarmes_history WHERE ($1::text IS NULL OR "TIPO_TA" = $1::text)
-                                             AND ($2::text IS NULL OR "TIPO_REDE" = $2::text)`;
+  const query = `SELECT * FROM alarmes_history 
+                 WHERE ($1::text IS NULL OR "TIPO_TA" = $1::text)
+                 AND ($2::text IS NULL OR "TIPO_REDE" = $2::text)
+                 AND ($3::text IS NULL OR "ESTADO" = $3::text)
+                 AND ($4::text IS NULL OR "LOCALIDADE" = $4::text)
+                 AND ($5::text IS NULL OR "SITE" = $5::text)
+                 AND ($6::text IS NULL OR "TIPO_ALARME" = $6::text)
+                 AND ($7::text IS NULL OR "CLASSIFICACAO" = $7::text)
+                 AND ($8::timestamp IS NULL AND $9::timestamp IS NULL OR ("DATA_APRESENTACAO" BETWEEN $8::timestamp AND $9::timestamp))
+                 `;
 
-  const results = await alarmesRepository.query(query, [TIPO_TA, TIPO_REDE]);
-  console.log(results);
+  const results = await alarmesRepository.query(query, [
+    TIPO_TA,
+    TIPO_REDE,
+    ESTADO,
+    LOCALIDADE,
+    SITE,
+    TIPO_ALARME,
+    CLASSIFICACAO,
+    DATA_INICIO,
+    DATA_FIM,
+  ]);
   return results;
 };
 
