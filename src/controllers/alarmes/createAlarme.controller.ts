908a74f1bf9dfc7430c48c2264e4fb89608bb8d4
rodolfo <rodolfojaques13@gmail.com>
2023-11-01 11:03:32 -0300

    feat: criação de rotina de vocalização (progresso 40%)

--- src/controllers/alarmes/createAlarme.controller.ts
+++ src/controllers/alarmes/createAlarme.controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
 import { AppError, handleError } from "../../error/appError";
 import createAlarmeService from "../../services/alarmes/createAlarme.service";
-import vocalizacaoService from "../../services/vocalizacao/vocalizacao.service";
+import vocalizacaoService from "../../services/vocalizacao/grupoVocalizacao.service";
+import chamadaVocalizacaoService from "../../services/vocalizacao/chamadaVocalizacao.service";
 
 const createAlarmeController = async (req:Request,res:Response) => {
     try {
         const data = req.body
         const alarme = await createAlarmeService(data)
 
-        if(!!alarme) await vocalizacaoService(alarme)
+        if(!!alarme){
+            const getGrupo = await vocalizacaoService(alarme)
+            if(!!getGrupo) await chamadaVocalizacaoService(getGrupo)
+        }
 
         return res.status(201).json(alarme)
     } catch (error) {
---
+++ src/services/vocalizacao/chamadaVocalizacao.service.ts
@@ -0,0 +1,59 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const chamadaVocalizacaoService = async (data:any) => {
+    const output = {
+        contacts:[
+            {
+                name: data.plantonista.nome,
+                code: uuidv4(),
+                phones:[
+                    data.plantonista.tel_cel
+                ],
+                CLASSIFICACAO:data.alarme.CLASSIFICACAO,
+                TIPO_ALARME: data.alarme.TIPO_ALARME,
+                SITE: data.alarme.SITE,
+                LOCALIDADE: data.alarme.LOCALIDADE,
+                ESTADO: data.alarme.ESTADO,
+                TA: data.alarme.TA,
+                TIPO_TA: data.alarme.TIPO_TA,
+                TIPO_REDE: data.alarme.TIPO_REDE,
+                DATA_APRESENTACAO: data.alarme.DATA_APRESENTACAO,
+                status: true
+            }
+        ]
+    }
+
+    const chamadas = async () => {
+        console.log("axios mandou o post");
+        console.log();
+        const monitorando = output.contacts[0].code
+
+        setTimeout(() => {
+            console.log("acabou o tempo");            
+            console.log();            
+            console.log("plantonista não atendeu", " code: ", monitorando);
+            console.log();
+            console.log("proximo contado");
+            
+            setTimeout(() => {
+                console.log("acabou o tempo");            
+                console.log();            
+                console.log("Gerente1 não atendeu", " code: ", monitorando);
+                console.log();
+                console.log("proximo contado");
+                
+                setTimeout(() => {
+                    console.log("não atendido");
+                    
+                }, 30000);
+            }, 30000);
+        }, 30000);
+        
+    }
+    chamadas()
+
+    console.log(output);
+    
+}
+
+export default chamadaVocalizacaoService
