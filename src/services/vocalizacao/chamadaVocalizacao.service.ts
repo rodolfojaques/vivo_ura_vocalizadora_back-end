be5c32b17721f6677ac89061a302b3d246ab956a
rodolfo <rodolfojaques13@gmail.com>
2023-11-01 16:44:54 -0300

    feat: Reformulação de logica para vocalização

--- src/services/vocalizacao/chamadaVocalizacao.service.ts
+++ src/services/vocalizacao/chamadaVocalizacao.service.ts
@@ -4,9 +4,9 @@ import { VocalizacaoHistory } from "../../../src-new/entities/vocalizacao.histor
 import { ReturnVocalizacao } from "../../entities/returnVocalizacao.entity";
 
 const chamadaVocalizacaoService = async (data: any) => {
-  console.log("Cheguei para vocalizar");
   const vocalizacaoHistoryRepository =
     AppDataSourceHistory.getRepository(VocalizacaoHistory);
+
   const returnVocalizacaoRepository =
     AppDataSource.getRepository(ReturnVocalizacao);
 
@@ -19,6 +19,7 @@ const chamadaVocalizacaoService = async (data: any) => {
     lastRecord ? (code = lastRecord[0].id + 1) : (code = 1);
   };
   generateCode();
+
   const newData = vocalizacaoHistoryRepository.create({
     contact_id: data.plantonista.id,
     plantonista: data.plantonista.nome,
@@ -28,6 +29,7 @@ const chamadaVocalizacaoService = async (data: any) => {
   });
 
   await vocalizacaoHistoryRepository.save(newData);
+
   const output = {
     contacts: [
       {
@@ -55,123 +57,137 @@ const chamadaVocalizacaoService = async (data: any) => {
     });
     const monitorando = output.contacts[0].code;
 
-    const timeout1 = setTimeout(async () => {
+    setTimeout(async () => {
       const oneReturnVocalizacao = await returnVocalizacaoRepository.findOneBy({
         code: monitorando,
       });
-
+        console.log("terminou");
+        console.log("terminou");
+        console.log("terminou");
+        console.log("terminou");
+        console.log("terminou");
       if (oneReturnVocalizacao) {
         vocalizacaoHistoryRepository.update(oneReturnVocalizacao.id, {
           status: true,
         });
-        clearTimeout(timeout1);
-      }
-      console.log("acabou o tempo");
-      console.log();
-      console.log("plantonista não atendeu", " code: ", monitorando);
-      console.log();
-      console.log("proximo contado");
 
-      setTimeout(async () => {
-        generateCode();
-        const newData = vocalizacaoHistoryRepository.create({
-          contact_id: data.grupoAtuacao.id,
-          plantonista: data.grupoAtuacao.gerente1,
-          phone: data.grupoAtuacao.contato_ger1,
-          code: code,
-          alarme: data.alarme,
-        });
+        return
+        
+      } else {
+        setTimeout(async() => {
+            generateCode();
 
-        await vocalizacaoHistoryRepository.save(newData);
-        const output = {
-          contacts: [
-            {
-              name: data.grupoAtuacao.gerente1,
-              code: newData.code,
-              phones: [data.grupoAtuacao.contato_ger1],
-              CLASSIFICACAO: data.alarme.CLASSIFICACAO,
-              TIPO_ALARME: data.alarme.TIPO_ALARME,
-              SITE: data.alarme.SITE,
-              LOCALIDADE: data.alarme.LOCALIDADE,
-              ESTADO: data.alarme.ESTADO,
-              TA: data.alarme.TA,
-              TIPO_TA: data.alarme.TIPO_TA,
-              TIPO_REDE: data.alarme.TIPO_REDE,
-              DATA_APRESENTACAO: data.alarme.DATA_APRESENTACAO,
-            },
-          ],
-        };
-        axios.post("", output, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const monitorando = output.contacts[0].code;
-        const oneReturnVocalizacao =
-          await returnVocalizacaoRepository.findOneBy({
-            code: monitorando,
-          });
-        if (oneReturnVocalizacao) {
-          vocalizacaoHistoryRepository.update(oneReturnVocalizacao.id, {
-            status: true,
-          });
-          clearTimeout(timeout1);
+            const newData = vocalizacaoHistoryRepository.create({
+            contact_id: data.grupoAtuacao.id,
+            plantonista: data.grupoAtuacao.gerente1,
+            phone: data.grupoAtuacao.contato_ger1,
+            code: code,
+            alarme: data.alarme,
+            });
+
+            await vocalizacaoHistoryRepository.save(newData);
+            const output = {
+                contacts: [
+                    {
+                    name: data.grupoAtuacao.gerente1,
+                    code: newData.code,
+                    phones: [data.grupoAtuacao.contato_ger1],
+                    CLASSIFICACAO: data.alarme.CLASSIFICACAO,
+                    TIPO_ALARME: data.alarme.TIPO_ALARME,
+                    SITE: data.alarme.SITE,
+                    LOCALIDADE: data.alarme.LOCALIDADE,
+                    ESTADO: data.alarme.ESTADO,
+                    TA: data.alarme.TA,
+                    TIPO_TA: data.alarme.TIPO_TA,
+                    TIPO_REDE: data.alarme.TIPO_REDE,
+                    DATA_APRESENTACAO: data.alarme.DATA_APRESENTACAO,
+                    },
+                ],
+            };
+
+            axios.post("", output, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const monitorando = output.contacts[0].code;
+
+            const oneReturnVocalizacao =
+            await returnVocalizacaoRepository.findOneBy({
+                code: monitorando,
+            });
+
+            if (oneReturnVocalizacao) {
+                vocalizacaoHistoryRepository.update(oneReturnVocalizacao.id, {
+                    status: true,
+                });
+            } else {
+            setTimeout(async () => {
+                generateCode();
+                const newData = vocalizacaoHistoryRepository.create({
+                  contact_id: data.grupoAtuacao.id,
+                  plantonista: data.grupoAtuacao.gerente2,
+                  phone: data.grupoAtuacao.contato_ger2,
+                  code: code,
+                  alarme: data.alarme,
+                });
+      
+                await vocalizacaoHistoryRepository.save(newData);
+
+                const output = {
+                  contacts: [
+                    {
+                      name: data.grupoAtuacao.gerente2,
+                      code: newData.code,
+                      phones: [data.grupoAtuacao.contato_ger2],
+                      CLASSIFICACAO: data.alarme.CLASSIFICACAO,
+                      TIPO_ALARME: data.alarme.TIPO_ALARME,
+                      SITE: data.alarme.SITE,
+                      LOCALIDADE: data.alarme.LOCALIDADE,
+                      ESTADO: data.alarme.ESTADO,
+                      TA: data.alarme.TA,
+                      TIPO_TA: data.alarme.TIPO_TA,
+                      TIPO_REDE: data.alarme.TIPO_REDE,
+                      DATA_APRESENTACAO: data.alarme.DATA_APRESENTACAO,
+                    },
+                  ],
+                };
+
+                axios.post("", output, {
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                });
+
+                const monitorando = output.contacts[0].code;
+
+                const oneReturnVocalizacao =
+                  await returnVocalizacaoRepository.findOneBy({
+                    code: monitorando,
+                  });
+
+                if (oneReturnVocalizacao) {
+                  vocalizacaoHistoryRepository.update(oneReturnVocalizacao.id, {
+                    status: true,
+                  });
+                }
+                console.log("não atendido");
+              }, 30000);
         }
         console.log("acabou o tempo");
         console.log();
         console.log("Gerente1 não atendeu", " code: ", monitorando);
         console.log();
         console.log("proximo contado");
-
-        setTimeout(async () => {
-          generateCode();
-          const newData = vocalizacaoHistoryRepository.create({
-            contact_id: data.grupoAtuacao.id,
-            plantonista: data.grupoAtuacao.gerente2,
-            phone: data.grupoAtuacao.contato_ger2,
-            code: code,
-            alarme: data.alarme,
-          });
-
-          await vocalizacaoHistoryRepository.save(newData);
-          const output = {
-            contacts: [
-              {
-                name: data.grupoAtuacao.gerente2,
-                code: newData.code,
-                phones: [data.grupoAtuacao.contato_ger2],
-                CLASSIFICACAO: data.alarme.CLASSIFICACAO,
-                TIPO_ALARME: data.alarme.TIPO_ALARME,
-                SITE: data.alarme.SITE,
-                LOCALIDADE: data.alarme.LOCALIDADE,
-                ESTADO: data.alarme.ESTADO,
-                TA: data.alarme.TA,
-                TIPO_TA: data.alarme.TIPO_TA,
-                TIPO_REDE: data.alarme.TIPO_REDE,
-                DATA_APRESENTACAO: data.alarme.DATA_APRESENTACAO,
-              },
-            ],
-          };
-          axios.post("", output, {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-          const monitorando = output.contacts[0].code;
-          const oneReturnVocalizacao =
-            await returnVocalizacaoRepository.findOneBy({
-              code: monitorando,
-            });
-          if (oneReturnVocalizacao) {
-            vocalizacaoHistoryRepository.update(oneReturnVocalizacao.id, {
-              status: true,
-            });
-            clearTimeout(timeout1);
-          }
-          console.log("não atendido");
         }, 30000);
-      }, 30000);
-    }, 30000);
+      }
+    //   console.log("acabou o tempo");
+    //   console.log();
+    //   console.log("plantonista não atendeu", " code: ", monitorando);
+    //   console.log();
+    //   console.log("proximo contado");
+    }, 30000);    
   };
   chamadas();
 
