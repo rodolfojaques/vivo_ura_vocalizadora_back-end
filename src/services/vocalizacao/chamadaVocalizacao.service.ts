3ecd8dc4de9c16f51b6572821f99f9eefdbcfef9
rodolfo <rodolfojaques13@gmail.com>
2023-12-11 17:25:26 -0300

    feat: Criação de logica para fila chamadas

--- src/services/vocalizacao/chamadaVocalizacao.service.ts
+++ src/services/vocalizacao/chamadaVocalizacao.service.ts
@@ -2,15 +2,211 @@ import axios from "axios";
 import { AppDataSource, AppDataSourceHistory } from "../../data-source";
 import { VocalizacaoHistory } from "../../../src-new/entities/vocalizacao.history.entity";
 import { ReturnVocalizacao } from "../../entities/returnVocalizacao.entity";
+import { Usuarios } from "../../entities/usuarios.entity";
 
 const chamadaVocalizacaoService = async (data: any) => {
   const dtFormatado = data.alarme.DATA_APRESENTACAO.slice(0,10) + " " + data.alarme.DATA_APRESENTACAO.slice(11,19)
   
-  const vocalizacaoHistoryRepository =
-    AppDataSourceHistory.getRepository(VocalizacaoHistory);
+  const vocalizacaoHistoryRepository = AppDataSourceHistory.getRepository(VocalizacaoHistory);
 
-  const returnVocalizacaoRepository =
-    AppDataSource.getRepository(ReturnVocalizacao);
+  const returnVocalizacaoRepository = AppDataSource.getRepository(ReturnVocalizacao);
+
+  const usuarioRepository = AppDataSource.getRepository(Usuarios)
+
+  const userId = data.plantonista.id
+
+  const chamadas = async () => {
+
+    const usuario = await usuarioRepository.findOneBy({id:userId})
+
+    const newData = vocalizacaoHistoryRepository.create({
+      contact_id: data.plantonista.id,
+      plantonista: data.plantonista.nome,
+      phone: data.plantonista.tel_cel,
+      alarme: data.alarme,
+    });      
+
+    await vocalizacaoHistoryRepository.save(newData);
+
+    const output = {
+      contacts: [
+        {
+          name: data.plantonista.nome,
+          code: newData.code.toString(),
+          phones: [data.plantonista.tel_cel],            
+          ALARME: data.alarme.TIPO_ALARME,
+          SITE: data.alarme.SITE,
+          LOCALIDADE: data.alarme.LOCALIDADE,
+          ESTADO: data.alarme.ESTADO,
+          TA: data.alarme.TA.toString(),            
+          TIPO_REDE: data.alarme.TIPO_REDE,
+          DATA_HORA: dtFormatado,
+        },
+      ],
+    };
+    console.log(output);
+
+    axios.post("http://186.238.82.229/api/mailing/12/contacts?key=kKoD1X4n", output, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    const monitorando = output.contacts[0].code;
+
+    setTimeout(async () => {
+      if(usuario?.emEspera! <= 0){
+        await usuarioRepository.update(userId,{emChamada: false})
+        await usuarioRepository.update(userId,{emEspera: 0})
+      }
+
+      const oneReturnVocalizacao = await returnVocalizacaoRepository.findOneBy({
+        code: monitorando,
+      });
+
+      const historyVoc = await vocalizacaoHistoryRepository.findOneBy({
+        code: Number(monitorando)
+      })
+
+      if (oneReturnVocalizacao) {
+        if(historyVoc){
+          vocalizacaoHistoryRepository.update(historyVoc.id, {
+            status: true,
+          });            
+        }
+
+        return
+        
+      } else {              
+
+        const newData = vocalizacaoHistoryRepository.create({
+        contact_id: data.grupoAtuacao.id,
+        plantonista: data.grupoAtuacao.gerente1,
+        phone: data.grupoAtuacao.contato_ger1,
+        alarme: data.alarme,
+        });
+
+        await vocalizacaoHistoryRepository.save(newData);
+        const output = {
+            contacts: [
+                {
+                name: data.grupoAtuacao.gerente1,
+                code: newData.code.toString(),
+                phones: [data.grupoAtuacao.contato_ger1],                    
+                ALARME: data.alarme.TIPO_ALARME,
+                SITE: data.alarme.SITE,
+                LOCALIDADE: data.alarme.LOCALIDADE,
+                ESTADO: data.alarme.ESTADO,
+                TA: data.alarme.TA.toString(),                    
+                TIPO_REDE: data.alarme.TIPO_REDE,
+                DATA_HORA: dtFormatado,
+                },
+            ],
+        };
+        
+
+        axios.post("http://186.238.82.229/api/mailing/12/contacts?key=kKoD1X4n", output, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+
+        const monitorando = output.contacts[0].code;
+
+        setTimeout(async() => {
+
+          const oneReturnVocalizacao =
+          await returnVocalizacaoRepository.findOneBy({
+              code: monitorando,
+          });
+
+          const historyVoc = await vocalizacaoHistoryRepository.findOneBy({
+            code: Number(monitorando)
+          })
+
+          if (oneReturnVocalizacao) {  
+            if(historyVoc){
+              vocalizacaoHistoryRepository.update(historyVoc.id, {
+                status: true,
+              });            
+            }
+
+              return
+          } else {
+                
+            const newData = vocalizacaoHistoryRepository.create({
+              contact_id: data.grupoAtuacao.id,
+              plantonista: data.grupoAtuacao.gerente2,
+              phone: data.grupoAtuacao.contato_ger2,
+              alarme: data.alarme,
+            });
+  
+            await vocalizacaoHistoryRepository.save(newData);
+
+            const output = {
+              contacts: [
+                {
+                  name: data.grupoAtuacao.gerente2,
+                  code: newData.code.toString(),
+                  phones: [data.grupoAtuacao.contato_ger2],                      
+                  ALARME: data.alarme.TIPO_ALARME,
+                  SITE: data.alarme.SITE,
+                  LOCALIDADE: data.alarme.LOCALIDADE,
+                  ESTADO: data.alarme.ESTADO,
+                  TA: data.alarme.TA.toString(),                      
+                  TIPO_REDE: data.alarme.TIPO_REDE,
+                  DATA_HORA: dtFormatado,
+                },
+              ],
+            };
+
+            axios.post("http://186.238.82.229/api/mailing/12/contacts?key=kKoD1X4n", output, {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+
+            const monitorando = output.contacts[0].code;
+
+            setTimeout(async () => {
+
+              const oneReturnVocalizacao =
+                await returnVocalizacaoRepository.findOneBy({
+                  code: monitorando,
+                });
+
+                const historyVoc = await vocalizacaoHistoryRepository.findOneBy({
+                  code: Number(monitorando)
+                })
+
+              if (oneReturnVocalizacao) {
+                if(historyVoc){
+                  vocalizacaoHistoryRepository.update(historyVoc.id, {
+                    status: true,
+                  });            
+                }
+              }
+              console.log("acabou o tempo");
+              console.log();
+              console.log("Gerente2 não atendeu", " code: ", monitorando);
+              console.log();
+              console.log("não atendido");
+            }, 320000);
+          }
+          console.log("acabou o tempo");
+          console.log();
+          console.log("Gerente1 não atendeu", " code: ", monitorando);
+          console.log();
+          console.log("proximo contado");
+          }, 320000);
+      }
+      console.log("acabou o tempo");
+      console.log();
+      console.log("plantonista não atendeu", " code: ", monitorando);
+      console.log();
+      console.log("proximo contado");
+    }, 320000);    
+  };
 
   if(!data.grupoAtuacao || !data.plantonista){
     const newData = vocalizacaoHistoryRepository.create({
@@ -24,194 +220,34 @@ const chamadaVocalizacaoService = async (data: any) => {
 
     return
 
-  } else {
+  } else if(data.plantonista.emChamada){    
+    const emEspDefault = data.plantonista.emEspera +1
+    await usuarioRepository.update(userId,{emEspera: emEspDefault})
 
-    const chamadas = async () => {
-
-      const newData = vocalizacaoHistoryRepository.create({
-        contact_id: data.plantonista.id,
-        plantonista: data.plantonista.nome,
-        phone: data.plantonista.tel_cel,
-        alarme: data.alarme,
-      });      
-
-      await vocalizacaoHistoryRepository.save(newData);
-
-      const output = {
-        contacts: [
-          {
-            name: data.plantonista.nome,
-            code: newData.code.toString(),
-            phones: [data.plantonista.tel_cel],            
-            ALARME: data.alarme.TIPO_ALARME,
-            SITE: data.alarme.SITE,
-            LOCALIDADE: data.alarme.LOCALIDADE,
-            ESTADO: data.alarme.ESTADO,
-            TA: data.alarme.TA.toString(),            
-            TIPO_REDE: data.alarme.TIPO_REDE,
-            DATA_HORA: dtFormatado,
-          },
-        ],
-      };
-      console.log(output);
-
-      axios.post("http://186.238.82.229/api/mailing/12/contacts?key=kKoD1X4n", output, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-
-      const monitorando = output.contacts[0].code;
-
-      setTimeout(async () => {
-        const oneReturnVocalizacao = await returnVocalizacaoRepository.findOneBy({
-          code: monitorando,
-        });
-
-        const historyVoc = await vocalizacaoHistoryRepository.findOneBy({
-          code: Number(monitorando)
-        })
-
-        if (oneReturnVocalizacao) {
-          if(historyVoc){
-            vocalizacaoHistoryRepository.update(historyVoc.id, {
-              status: true,
-            });            
-          }
-
-          return
-          
-        } else {              
-
-          const newData = vocalizacaoHistoryRepository.create({
-          contact_id: data.grupoAtuacao.id,
-          plantonista: data.grupoAtuacao.gerente1,
-          phone: data.grupoAtuacao.contato_ger1,
-          alarme: data.alarme,
-          });
-
-          await vocalizacaoHistoryRepository.save(newData);
-          const output = {
-              contacts: [
-                  {
-                  name: data.grupoAtuacao.gerente1,
-                  code: newData.code.toString(),
-                  phones: [data.grupoAtuacao.contato_ger1],                    
-                  ALARME: data.alarme.TIPO_ALARME,
-                  SITE: data.alarme.SITE,
-                  LOCALIDADE: data.alarme.LOCALIDADE,
-                  ESTADO: data.alarme.ESTADO,
-                  TA: data.alarme.TA.toString(),                    
-                  TIPO_REDE: data.alarme.TIPO_REDE,
-                  DATA_HORA: dtFormatado,
-                  },
-              ],
-          };
-          
-
-          axios.post("http://186.238.82.229/api/mailing/12/contacts?key=kKoD1X4n", output, {
-              headers: {
-                  "Content-Type": "application/json",
-              },
-          })
-
-          const monitorando = output.contacts[0].code;
-
-          setTimeout(async() => {
-
-            const oneReturnVocalizacao =
-            await returnVocalizacaoRepository.findOneBy({
-                code: monitorando,
-            });
-
-            const historyVoc = await vocalizacaoHistoryRepository.findOneBy({
-              code: Number(monitorando)
-            })
-
-            if (oneReturnVocalizacao) {  
-              if(historyVoc){
-                vocalizacaoHistoryRepository.update(historyVoc.id, {
-                  status: true,
-                });            
-              }
-
-                return
-            } else {
-                  
-              const newData = vocalizacaoHistoryRepository.create({
-                contact_id: data.grupoAtuacao.id,
-                plantonista: data.grupoAtuacao.gerente2,
-                phone: data.grupoAtuacao.contato_ger2,
-                alarme: data.alarme,
-              });
+    const usuario = await usuarioRepository.findOneBy({id:userId})
     
-              await vocalizacaoHistoryRepository.save(newData);
+    let delay = 130000 //ms
+    let qtdEspera = usuario?.emEspera!
 
-              const output = {
-                contacts: [
-                  {
-                    name: data.grupoAtuacao.gerente2,
-                    code: newData.code.toString(),
-                    phones: [data.grupoAtuacao.contato_ger2],                      
-                    ALARME: data.alarme.TIPO_ALARME,
-                    SITE: data.alarme.SITE,
-                    LOCALIDADE: data.alarme.LOCALIDADE,
-                    ESTADO: data.alarme.ESTADO,
-                    TA: data.alarme.TA.toString(),                      
-                    TIPO_REDE: data.alarme.TIPO_REDE,
-                    DATA_HORA: dtFormatado,
-                  },
-                ],
-              };
+    setTimeout(async () => {
+      await usuarioRepository.update(userId,{emEspera: (usuario?.emEspera! -1)})
+      const usuarioUp = await usuarioRepository.findOneBy({id:userId})
 
-              axios.post("http://186.238.82.229/api/mailing/12/contacts?key=kKoD1X4n", output, {
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              })
+      if(usuarioUp?.emEspera! <= 0){
+        await usuarioRepository.update(userId,{emEspera: 0})
+        await usuarioRepository.update(userId,{emChamada: false})
+        
+        chamadas()
+      } else {
+        chamadas()
+      }
 
-              const monitorando = output.contacts[0].code;
+    }, delay * qtdEspera);
 
-              setTimeout(async () => {
+  } else {
+    await usuarioRepository.update(userId,{emChamada: true})
 
-                const oneReturnVocalizacao =
-                  await returnVocalizacaoRepository.findOneBy({
-                    code: monitorando,
-                  });
-
-                  const historyVoc = await vocalizacaoHistoryRepository.findOneBy({
-                    code: Number(monitorando)
-                  })
-
-                if (oneReturnVocalizacao) {
-                  if(historyVoc){
-                    vocalizacaoHistoryRepository.update(historyVoc.id, {
-                      status: true,
-                    });            
-                  }
-                }
-                console.log("acabou o tempo");
-                console.log();
-                console.log("Gerente2 não atendeu", " code: ", monitorando);
-                console.log();
-                console.log("não atendido");
-              }, 320000);
-            }
-            console.log("acabou o tempo");
-            console.log();
-            console.log("Gerente1 não atendeu", " code: ", monitorando);
-            console.log();
-            console.log("proximo contado");
-            }, 320000);
-        }
-        console.log("acabou o tempo");
-        console.log();
-        console.log("plantonista não atendeu", " code: ", monitorando);
-        console.log();
-        console.log("proximo contado");
-      }, 320000);    
-    };
-    chamadas();    
+    chamadas()   
   }
 };
 
